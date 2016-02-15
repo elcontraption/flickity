d684a98453dd99e7746a5cbe3d8326ef5c1aef76
Darin Reid <darinreid@gmail.com>
2016-02-15 07:01:16 -0800

    Initial commit

---
+++ js/cell.js
@@ -0,0 +1,99 @@
+( function( window, factory ) {
+  'use strict';
+  // universal module definition
+
+  if ( typeof define == 'function' && define.amd ) {
+    // AMD
+    define( [
+      'get-size/get-size'
+    ], function( getSize ) {
+      return factory( window, getSize );
+    });
+  } else if ( typeof exports == 'object' ) {
+    // CommonJS
+    module.exports = factory(
+      window,
+      require('get-size')
+    );
+  } else {
+    // browser global
+    window.Flickity = window.Flickity || {};
+    window.Flickity.Cell = factory(
+      window,
+      window.getSize
+    );
+  }
+
+}( window, function factory( window, getSize ) {
+
+'use strict';
+
+function Cell( elem, index, parent ) {
+  this.element = elem;
+  this.index = index;
+  this.parent = parent;
+
+  this.create();
+}
+
+var isIE8 = 'attachEvent' in window;
+
+Cell.prototype.create = function() {
+  this.element.style.position = 'absolute';
+  // IE8 prevent child from changing focus http://stackoverflow.com/a/17525223/182183
+  if ( isIE8 ) {
+    this.element.setAttribute( 'unselectable', 'on' );
+  }
+  this.x = 0;
+  this.shift = 0;
+};
+
+Cell.prototype.destroy = function() {
+  // reset style
+  this.element.style.position = '';
+  var side = this.parent.originSide;
+  this.element.style[ side ] = '';
+};
+
+Cell.prototype.getSize = function() {
+  this.size = getSize( this.element );
+};
+
+Cell.prototype.setPosition = function( x ) {
+  this.x = x;
+  this.setDefaultTarget();
+  this.renderPosition( x );
+  this.element.style['z-index'] = 0 - this.index;
+
+  if (this.parent.options.transitionEffect) {
+    this.parent.on('positionSlider', this.parent.options.transitionEffect.bind(this));
+  }
+};
+
+Cell.prototype.setDefaultTarget = function() {
+  var marginProperty = this.parent.originSide == 'left' ? 'marginLeft' : 'marginRight';
+  this.target = this.x + this.size[ marginProperty ] +
+    this.size.width * this.parent.cellAlign;
+};
+
+Cell.prototype.renderPosition = function( x ) {
+  // render position of cell with in slider
+  var side = this.parent.originSide;
+  this.element.style[ side ] = this.parent.getPositionValue( x );
+};
+
+/**
+ * @param {Integer} factor - 0, 1, or -1
+**/
+Cell.prototype.wrapShift = function( shift ) {
+  this.shift = shift;
+  this.renderPosition( this.x + this.parent.slideableWidth * shift );
+};
+
+Cell.prototype.remove = function() {
+  this.element.parentNode.removeChild( this.element );
+};
+
+return Cell;
+
+}));
